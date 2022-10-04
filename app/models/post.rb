8587f9fe872294433c3295887a405122ce292723
billymwangi <billy.mwangi@student.moringaschool.com>
2022-10-04 14:57:48 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title_contains_clickbait
+
+    TITLE_CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top/i,
+        /Guess/i
+
+    ]
+    def title_contains_clickbait
+        if TITLE_CLICKBAIT.none? { |word| word.match title}
+            errors.add(:title, "Title not clickbaity enough")
+        end
+    end
 end
